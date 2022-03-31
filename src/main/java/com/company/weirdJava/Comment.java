9a0abcdc6f1d0c3ee016f632522f26e091bf6d78
chaenabi <find1086@gmail.com>
2022-04-01 04:26:17 +0900

    remove unnecessary unicode

--- src/main/java/com/company/weirdJava/Comment.java
+++ src/main/java/com/company/weirdJava/Comment.java
@@ -2,23 +2,16 @@ package com.company.weirdJava;
 
 public class Comment {
     public static void main(String[] args) {
-
-        int number = 0;
+        int number = 1;
         System.out.println(number);
-        // number = 1;
+        // number = 2;
         System.out.println(number);
-        // \u000a number = 2;
+        // \u000a number = 3;
         System.out.println(number);
-        /*/*$/%\u0126//^\u002A\u002F
-
-        number = 3;
-
-
-
+        /*
+           \u002A\u002F number = 4;
+           System.out.println("범위형 주석 무효화 확인");
         //*/
-
-
         System.out.println(number);
-
     }
 }
